387dc601cc07c490a12199b94d800d1140fed40b
sa77irak <sa77irak@gmail.com>
2019-09-08 13:19:57 +0900

    MichaelSukkiSong

---
+++ index.js
@@ -0,0 +1,6 @@
+// To use node modules(like fs to read files), we require them into our code and store the result in a variable.
+const fs = require(fs);
+
+const hello = 'Hello world';
+console.log(hello);
+
