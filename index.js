73471687b6a05dfe0555b1e5306eac68e5c42f4d
sa77irak <sa77irak@gmail.com>
2019-09-10 15:22:01 +0900

    creating a simple web server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // To use node modules(like fs to read files), we require them into our code and store the result in a variable.
 const fs = require('fs');
+// http gives us networking capability, such as building a server.
+const http = require('http');
+
+
+////////////////////////////////
+// FILES
 
 /*
 // Blocking, synchronus way
@@ -10,6 +16,7 @@ fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written!');
 */
 
+/*
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     if (err) return console.log('ERROR! :(');
@@ -26,5 +33,34 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     });
 });
 console.log('Will read file!');
+*/
+
+
+
+////////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
