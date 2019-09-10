16b536ee96704aaa682fbd5558257f481f0291ee
sa77irak <sa77irak@gmail.com>
2019-09-10 15:56:49 +0900

    routing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 // http gives us networking capability, such as building a server.
 const http = require('http');
+// for routing
+const url = require('url');
 
 
 ////////////////////////////////
@@ -40,12 +42,24 @@ console.log('Will read file!');
 ////////////////////////////////
 // SERVER
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the OVERVIEW');
+    } else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
-})
+});
 
 
 
