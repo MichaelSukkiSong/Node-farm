2b1abde7e7b4fa5d60b7c3b59b53c9cf1ca1ef26
sa77irak <sa77irak@gmail.com>
2019-09-11 15:17:59 +0900

    setting up prettier in vscode

--- index.js
+++ index.js
@@ -6,18 +6,12 @@ const http = require('http');
 // for routing
 const url = require('url');
 
-
-
 // third party modules
 const slugify = require('slugify');
-
-
 
 // our own modules
 // import modules
 const replaceTemplate = require('./modules/replaceTemplate');
-
-
 
 ////////////////////////////////
 // FILES
@@ -50,85 +44,70 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 console.log('Will read file!');
 */
 
-
-
 ////////////////////////////////
 // SERVER
 
-
-
 // this part is executed once, when the app is executed. so sync is fine.
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map(el => slugify(el.productName, { lower: true}));
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 console.log(slugs);
-
 
 // this is the part where the callback functons is called multiple times, everytime a user trys to access the server.
 const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);
-    //console.log(url.parse(req.url, true));
+  const { query, pathname } = url.parse(req.url, true);
+  //console.log(url.parse(req.url, true));
 
-    // Overview page
-    if(pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {
-            'Content-type': 'text/html',
-        });
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
 
-        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-        res.end(output);
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     // Product page
-    } else if (pathname === '/product') {
-        res.writeHead(200, {
-            'Content-type': 'text/html',
-        });
-        const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct, product);        
-        res.end(output);
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API
-    } else if (pathname === '/api') {
-
-        res.writeHead(200, {
-            'Content-type': 'application/json'
-        });
-        res.end(data);
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
 
     // Not fount
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        });
-        res.end('<h1>Page not found!</h1>');
-    }
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
 });
 
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
+  console.log('Listening to requests on port 8000');
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
